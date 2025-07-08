4cd2c4f4033552f4455a0f0d7a661b04026b22ce
Udemy-helen <dohoaian1212@gmail.com>
2025-07-09 00:04:38 +0700

    BTVN-Lesson2 : ex1, ex2

---
+++ tests/lesson-2/ex2.js
@@ -0,0 +1,8 @@
+// const myName = "Alex";
+// myName = "Nagi"; // myName là constant nên không thể gán lại hằng số. 
+// console.log(myName)
+
+let myName = "Alex";
+myName = "Nagi";           // let có thể thay đổi
+console.log(myName);       
+
